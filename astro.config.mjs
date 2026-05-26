c53a5d3a393c2bc27aa37c5fa1c521670fb59968
franzbu <54638185+franzbu@users.noreply.github.com>
2026-05-26 19:57:49 +0200

    Update landing page title and add Gitanjali subpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'static', // Static is best for a text/poem site
+  output: 'static',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
