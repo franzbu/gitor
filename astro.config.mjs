e5d7f5cf80e70e378e7ba24c9ab380c7b2ddb3c9
franzbu <54638185+franzbu@users.noreply.github.com>
2026-05-31 20:55:33 +0200

    added robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://gitor.uk',
+  integrations: [sitemap()],
   output: 'static',
   adapter: cloudflare({
     platformProxy: {
